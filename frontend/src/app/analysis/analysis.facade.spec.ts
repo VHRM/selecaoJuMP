12370a36376563bb61dd7cb8720608b2b2a33e70
Vladson Marinho <vhrm@cin.ufpe.br>
2023-05-26 19:25:34 -0300

    test files fixed

--- frontend/src/app/analysis/analysis.facade.spec.ts
+++ frontend/src/app/analysis/analysis.facade.spec.ts
@@ -1,7 +1,14 @@
+import { TestBed } from '@angular/core/testing';
 import { AnalysisFacade } from './analysis.facade';
 
 describe('AnalysisFacade', () => {
+  let facade: AnalysisFacade;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    facade = TestBed.inject(AnalysisFacade)
+  })
   it('should create an instance', () => {
-    expect(new AnalysisFacade()).toBeTruthy();
+    expect(facade).toBeTruthy();
   });
 });
